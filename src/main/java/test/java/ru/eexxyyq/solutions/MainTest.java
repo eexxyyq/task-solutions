71b94fa7f1f7d300a17025fe63a301a22aa8244b
eexxyyq <eexxyyq@gmail.com>
2021-09-04 18:02:04 +0300

    test init

---
+++ src/main/java/test/java/ru/eexxyyq/solutions/MainTest.java
@@ -0,0 +1,12 @@
+package test.java.ru.eexxyyq.solutions;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class MainTest {
+
+    @Test
+    void test() {
+        Assertions.assertEquals(1, 1);
+    }
+}
